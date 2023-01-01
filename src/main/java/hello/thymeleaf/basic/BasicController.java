c2d10460f22d9e8d3cb40920c008c852c50c23cb
min1925k <min1925k@gmail.com>
2023-01-01 23:53:15 +0900

    Print Text
    - th:text, [[]] : print text
    - th:utext, [()] : unescape

---
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -0,0 +1,66 @@
+package hello.thymeleaf.basic;
+
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+	/*
+	HTML 앤티티
+	- thymeleaf 가 <> 를 태그가 아니라 문자로 인식하고 출력한다.
+	- HTML 에서 사용하는 특수 문자를 HTML 엔티티로 변경하는 것을 이스케이프라 한다
+	- Unescape 가 존재한다.. [()], th:utext
+	 */
+	@GetMapping("/text-basic")
+	public String textBasic(Model model) {
+		model.addAttribute("data", "<b>Hello</b> Spring");
+		return "basic/text-basic";
+	}
+
+	@GetMapping("/text-unescaped")
+	public String textUnescaped(Model model) {
+		model.addAttribute("data", "<b>Hello</b> Spring");
+		return "basic/text-unescaped";
+	}
+
+	@GetMapping("/variable")
+	public String variable(Model model) {
+		User userA = new User("userA", 10);
+		User userB = new User("userB", 20);
+
+		List<User> list = new ArrayList<>();
+		list.add(userA);
+		list.add(userB);
+
+		Map<String, User> map = new HashMap<>();
+		map.put("userA", userA);
+		map.put("userB", userB);
+
+		model.addAttribute("user", userA);
+		model.addAttribute("users", list);
+		model.addAttribute("userMap", map);
+
+		return "basic/variable";
+	}
+
+	@Data
+	static class User{
+		private String username;
+		private int age;
+
+		public User(String username, int age) {
+			this.username = username;
+			this.age = age;
+		}
+	}
+}
