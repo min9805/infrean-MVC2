333aca6d635a7ad340d5650f1bf99d09e7b39ee6
min1925k <min1925k@gmail.com>
2023-01-02 00:42:00 +0900

    Basic Objects, Convenience Objects
    - Basic : #request, #response, #session, #servletContext, #locale
    - Convenience : param, session, SpringBean

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,6 +1,8 @@
 package hello.thymeleaf.basic;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -53,8 +55,21 @@ public class BasicController {
 		return "basic/variable";
 	}
 
+	@GetMapping("/basic-objects")
+	public String basicObjects(HttpSession session) {
+		session.setAttribute("sessionData", "Hello Session");
+		return "basic/basic-objects";
+	}
+
+	@Component("helloBean")
+	static class HelloBean {
+		public String hello(String data) {
+			return "hello" + data;
+		}
+	}
+
 	@Data
-	static class User{
+	static class User {
 		private String username;
 		private int age;
 
