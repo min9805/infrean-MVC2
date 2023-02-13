f3edd08cf4a3e91b407ab9490ab2980d01a15ad9
min1925k <min1925k@gmail.com>
2023-02-13 17:25:03 +0900

    스프링 인터셉터
    - 서블릿 필터와 비슷하게 공통 관심 사항을 처리한다.
    - 인터셉터는 어떤 컨트롤러가 호출되는지, 어떤 모델이 반환되는 지 응답 정보도 받을 수 있다
    - 로그 인터셉터

--- login/src/main/java/hello/login/WebConfig.java
+++ login/src/main/java/hello/login/WebConfig.java
@@ -1,33 +1,38 @@
 package hello.login;
 
-import hello.login.web.filter.LogFilter;
-import hello.login.web.filter.LoginCheckFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
+import hello.login.web.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
-
-import javax.servlet.Filter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-	@Bean
-	public FilterRegistrationBean logFilter() {
-		FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
-		filterFilterRegistrationBean.setFilter(new LogFilter());
-		filterFilterRegistrationBean.setOrder(1);
-		filterFilterRegistrationBean.addUrlPatterns("/*");
-
-		return filterFilterRegistrationBean;
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LogInterceptor())
+				.order(1)
+				.addPathPatterns("/**")
+				.excludePathPatterns("/css/**", "/*.ico", "/error");
 	}
 
-	@Bean
-	public FilterRegistrationBean loginCheckFilter() {
-		FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
-		filterFilterRegistrationBean.setFilter(new LoginCheckFilter());
-		filterFilterRegistrationBean.setOrder(2);
-		filterFilterRegistrationBean.addUrlPatterns("/*");
-
-		return filterFilterRegistrationBean;
-	}
+//	@Bean
+//	public FilterRegistrationBean logFilter() {
+//		FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+//		filterFilterRegistrationBean.setFilter(new LogFilter());
+//		filterFilterRegistrationBean.setOrder(1);
+//		filterFilterRegistrationBean.addUrlPatterns("/*");
+//
+//		return filterFilterRegistrationBean;
+//	}
+//
+//	@Bean
+//	public FilterRegistrationBean loginCheckFilter() {
+//		FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+//		filterFilterRegistrationBean.setFilter(new LoginCheckFilter());
+//		filterFilterRegistrationBean.setOrder(2);
+//		filterFilterRegistrationBean.addUrlPatterns("/*");
+//
+//		return filterFilterRegistrationBean;
+//	}
 }
